fd3b985c1e56dd4f49813443e8b7079d4332a5b7
wwdfdo <wwdfdo@gmail.com>
2023-04-11 21:11:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Transition from "react-transition-group/Transition";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+import Modal from "./components/Modal/Modal";
+import Backdrop from "./components/Backdrop/Backdrop";
+import List from "./components/List/List";
+
+class App extends Component {
+  state = {
+    modalIsOpen: false,
+    showBlock: false,
+  };
+
+  showModal = () => {
+    this.setState({ modalIsOpen: true });
+  };
+
+  closeModal = () => {
+    this.setState({ modalIsOpen: false });
+  };
+
+  render() {
+    return (
+      <div>
+        <div className="App">
+          <h1>React Animations</h1>
+          <button
+            className="Button"
+            onClick={() =>
+              this.setState((prevState) => ({
+                showBlock: !prevState.showBlock,
+              }))
+            }
+          >
+            Toggle
+          </button>
+          <br />
+          <Transition
+            in={this.state.showBlock}
+            timeout={1000}
+            mountOnEnter
+            unmountOnExit
+            onEnter={() => console.log("onEnter")}
+            onEntering={() => console.log("onEntering")}
+            onEntered={() => console.log("onEntered")}
+            onExit={() => console.log("onExit")}
+            onExiting={() => console.log("onExiting")}
+            onExited={() => console.log("onExited")}
+          >
+            {(state) => (
+              <div
+                style={{
+                  backgroundColor: "red",
+                  width: 100,
+                  height: 100,
+                  margin: "auto",
+                  transition: "opacity 1s ease-out",
+                  opacity: state === "exiting" ? 0 : 1,
+                }}
+              />
+            )}
+          </Transition>
+          <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
+          {this.state.modalIsOpen ? <Backdrop show /> : null}
+          <button className="Button" onClick={this.showModal}>
+            Open Modal
+          </button>
+          <h3>Animating Lists</h3>
+          <List />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
